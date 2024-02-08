b0541b0b18c9d5eaf8c71d8999d65773c09308e7
Hi-tech <Hi-tech@DESKTOP-K8PC2B7>
2024-02-08 18:48:14 +0200

    Добавьте файлы проекта.

---
+++ Notebook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Notebook
+    {
+        
+        protected string title; // Название
+        protected int k; // Количество листов
+
+        
+        public Notebook(string title, int numberOfSheets)
+        {
+            this.title = title;
+            this.k = numberOfSheets;
+        }
+
+        
+        public void DisplayInformation()
+        {
+            Console.WriteLine($"заголовок: {title}");
+            Console.WriteLine($"кол во листов: {k}");
+        }
+
+       
+        public double CalculateCost()
+        {
+            return 15 * k;
+        }
+    }
+
+    class CommonNotebook : Notebook
+    {
+        
+        private string coverMaterial; // Материал обложки
+
+        
+        public CommonNotebook(string title, int numberOfSheets, string coverMaterial)
+            : base(title, numberOfSheets)
+        {
+            this.coverMaterial = coverMaterial;
+        }
+
+        
+        public void DisplayInformationAboutChild()
+        {
+            DisplayInformation();
+            Console.WriteLine($"материал покрытия: {coverMaterial}");
+        }
+
+        
+        public new double CalculateCost()
+        {
+            return base.CalculateCost() + 50;
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Notebook baseNotebook = new Notebook("Простой блокнот", 80);
+
+            
+            Console.WriteLine("информация о тетради:");
+            baseNotebook.DisplayInformation();
+            Console.WriteLine($"расходы: {baseNotebook.CalculateCost()} рублей.");
+
+            
+
+           
+            CommonNotebook commonNotebook = new CommonNotebook("красная тетрадь", 100, "пластик");
+
+            
+            Console.WriteLine("Информация об общей тетради:");
+            commonNotebook.DisplayInformationAboutChild();
+            Console.WriteLine($"Расходы: {commonNotebook.CalculateCost()} рублей.");
+            Console.ReadKey();
+        }
+    }
+}
+    
